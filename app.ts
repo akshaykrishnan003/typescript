372f807f16268082701b20f8e25768a0019c908b
akshaykrishnan003 <akshaykrishnan@ceknpy.ac.in>
2021-06-19 17:32:12 +0530

    enum

--- app.ts
+++ app.ts
@@ -1,1 +1,28 @@
-console.log('this is a test code');
+// Automatically enumerated constant identifier
+// human readable identifiers
+// custom type
+
+enum Role {
+  ADMIN,
+  READ_ONLY,
+  AUTHOR,
+}
+
+/* 
+its also possible to assign custom values 
+enum Role {
+  ADMIN = "ADMIN",
+  READ_ONLY = 100,
+  AUTHOR = "author",
+} */
+
+const person = {
+  name: "Tom",
+  age: 15,
+  hobbies: ["Sports", "Cooking"],
+  role: Role.ADMIN,
+};
+
+if (person.role === Role.ADMIN) {
+  console.log(person.name + " is admin");
+}
