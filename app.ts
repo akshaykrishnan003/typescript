3d1dcca47a0c956296d39cd9d006de360a3ec478
akshaykrishnan003 <akshaykrishnan@ceknpy.ac.in>
2021-06-19 19:31:04 +0530

    name change

--- app.ts
+++ app.ts
@@ -1,20 +1,20 @@
-function add(n1: number, n2: number) {
+function addFunctionOne(n1: number, n2: number) {
   // return type number is type inference'ed
   return n1 + n2;
 }
 
-function printResult(num: number): void {
+function printResultFunctionOne(num: number): void {
   // returns nothing
   console.log("Result: " + num);
 }
 
-function retUndefined(): undefined {
+function retUndefinedFunctionOne(): undefined {
   // returns Undefined
   console.log("returns undefinde");
   return undefined;
 }
 
-printResult(add(5, 12));
-retUndefined();
-console.log(printResult(add(5, 12))); // gives undefined
+printResultFunctionOne(addFunctionOne(5, 12));
+retUndefinedFunctionOne();
+console.log(printResultFunctionOne(addFunctionOne(5, 12))); // gives undefined
 // undefined is a type
