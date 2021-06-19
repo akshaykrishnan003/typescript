380b1c0957e98993ade54b3ddc4a38ea5cd8c823
akshaykrishnan003 <akshaykrishnan@ceknpy.ac.in>
2021-06-19 18:12:41 +0530

    alias and custom types

--- app.ts
+++ app.ts
@@ -1,1 +1,28 @@
-console.log('this is a test code');
+type Combinable = number | string; // any types or literals
+type ConversionDescriptor = "as-number" | "as-text";
+
+function combine(
+  inputOne: Combinable,
+  inputTwo: Combinable,
+  resultType: ConversionDescriptor
+) {
+  let result;
+  if (
+    (typeof inputOne === "number" && typeof inputTwo === "number") ||
+    resultType === "as-number"
+  ) {
+    result = +inputOne + +inputTwo;
+  } else {
+    result = inputOne.toString() + inputTwo.toString();
+  }
+  return result;
+}
+
+const combinedNumbers = combine(30, 26, "as-number");
+console.log(combinedNumbers);
+
+const combinedStringNumbers = combine("30", "26", "as-number");
+console.log(combinedStringNumbers);
+
+const combinedNames = combine("Max", "Anna", "as-text");
+console.log(combinedNames);
