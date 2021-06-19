9128398653b6185619538e8d3821f840066ca611
akshaykrishnan003 <akshaykrishnan@ceknpy.ac.in>
2021-06-19 17:13:19 +0530

    arrays

--- app.ts
+++ app.ts
@@ -1,1 +1,16 @@
-console.log('this is a test code');
+const person = {
+  name: "Tom",
+  age: 15,
+  hobbies: ["Sports", "Cooking"],
+};
+
+// let favoriteActivities: any[]; can have a array of any type
+let favoriteActivities: string[];
+favoriteActivities = ["sports"];
+// favoriteActivities = ["sports", 1]; ** will give error
+
+console.log(person.name);
+
+for (const hobby of person.hobbies) {
+  console.log(hobby.toUpperCase()); //since hobby is string as we set it in the object
+}
