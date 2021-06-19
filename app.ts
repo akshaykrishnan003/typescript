8b3e61f327d48a3ea228545746b9976ea15e9e97
akshaykrishnan003 <akshaykrishnan@ceknpy.ac.in>
2021-06-19 17:36:12 +0530

    datatype: any

--- app.ts
+++ app.ts
@@ -1,1 +1,5 @@
-console.log('this is a test code');
+// Any kind of value, no specific type assignment to variable
+
+//try to avoid at max
+let anyArray: any[]; //array of any type
+anyArray = ["one", 2, 3.0, "FOUR"];
