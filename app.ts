ec3e9a1cbe9d8261217d98bbfc61df4894ec3629
akshaykrishnan003 <akshaykrishnan@ceknpy.ac.in>
2021-06-19 19:34:44 +0530

    name change

--- app.ts
+++ app.ts
@@ -1,21 +1,21 @@
-function add(n1: number, n2: number) {
+function addFunctionTwo(n1: number, n2: number) {
   return n1 + n2;
 }
 
-function printResult(num: number): void {
+function printResultFunctionTwo(num: number): void {
   console.log("Result: " + num);
 }
 
-/* let combineValues: Function;
-combineValues = add;
-combineValues = printResult; works but defeats purpose
-combineValues = 5; !!! Error !!!
+/* let combineValuesFunctionTwo: Function;
+combineValuesFunctionTwo = add;
+combineValuesFunctionTwo = printResult; works but defeats purpose
+combineValuesFunctionTwo = 5; !!! Error !!!
 
-console.log(combineValues(8, 8));
+console.log(combineValuesFunctionTwo(8, 8));
  */
 
-let combineValues: (a: number, b: number) => number; // any fumction with parameter of types number and return type of number
-combineValues = add;
-// combineValues = printResult; !!! Error !!! since number of argument dont match as well as the return type
+let combineValuesFunctionTwo: (a: number, b: number) => number; // any fumction with parameter of types number and return type of number
+combineValuesFunctionTwo = addFunctionTwo;
+// combineValuesFunctionTwo = printResult; !!! Error !!! since number of argument dont match as well as the return type
 
-console.log(combineValues(8, 8));
+console.log(combineValuesFunctionTwo(8, 8));
