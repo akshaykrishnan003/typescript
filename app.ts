9d8840723c5b84fd3c923d12efaa8ce8e6546a31
akshaykrishnan003 <akshaykrishnan@ceknpy.ac.in>
2021-06-19 17:51:33 +0530

    union types

--- app.ts
+++ app.ts
@@ -1,1 +1,17 @@
-console.log('this is a test code');
+//run time types check are not always needed
+
+function combine(inputOne: number | string, inputTwo: number | string) {
+  let result;
+  if (typeof inputOne === "number" && typeof inputTwo === "number") {
+    result = inputOne + inputTwo;
+  } else {
+    result = inputOne.toString() + inputTwo.toString();
+  }
+  return result;
+}
+
+const combinedNumbers = combine(30, 26);
+console.log(combinedNumbers);
+
+const combinedNames = combine("Max", "Anna");
+console.log(combinedNames);
