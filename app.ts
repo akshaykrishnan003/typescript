347291e7585a88d9700602e166b9908f3d9c30f9
akshaykrishnan003 <akshaykrishnan@ceknpy.ac.in>
2021-06-19 18:08:12 +0530

    literals

--- app.ts
+++ app.ts
@@ -1,1 +1,68 @@
-console.log('this is a test code');
+/* 
+use case
+function combine(
+  inputOne: number | string,
+  inputTwo: number | string,
+  resultType: string
+) {
+  let result;
+  if (
+    (typeof inputOne === "number" && typeof inputTwo === "number") ||
+    resultType === "as-number"
+  ) {
+    result = +inputOne + +inputTwo;
+  } else {
+    result = inputOne.toString() + inputTwo.toString();
+  }
+  return result;
+ //   if (resultType === "as-number") {
+ //   return +result;
+ // } else {
+ //   return result.toString();
+ // }
+}
+
+const combinedNumbers = combine(30, 26, "as-number");
+console.log(combinedNumbers);
+
+const combinedStringNumbers = combine("30", "26", "as-number");
+console.log(combinedStringNumbers);
+
+const combinedNames = combine("Max", "Anna", "as-text");
+console.log(combinedNames);
+ */
+
+function combine(
+  inputOne: number | string,
+  inputTwo: number | string,
+  resultType: "as-number" | "as-text"
+) {
+  let result;
+  /* if (
+    (typeof inputOne === "number" && typeof inputTwo === "number") ||
+    resultType === "as-numb" !!! Error !!!
+  ) { */
+  if (
+    (typeof inputOne === "number" && typeof inputTwo === "number") ||
+    resultType === "as-number"
+  ) {
+    result = +inputOne + +inputTwo;
+  } else {
+    result = inputOne.toString() + inputTwo.toString();
+  }
+  return result;
+  //   if (resultType === "as-number") {
+  //   return +result;
+  // } else {
+  //   return result.toString();
+  // }
+}
+
+const combinedNumbers = combine(30, 26, "as-number");
+console.log(combinedNumbers);
+
+const combinedStringNumbers = combine("30", "26", "as-number");
+console.log(combinedStringNumbers);
+
+const combinedNames = combine("Max", "Anna", "as-text");
+console.log(combinedNames);
