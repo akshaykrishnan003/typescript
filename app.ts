250b8ff90ded677a2051bf45779856dbfc7e2812
akshaykrishnan003 <akshaykrishnan@ceknpy.ac.in>
2021-06-19 18:31:29 +0530

    function return types

--- app.ts
+++ app.ts
@@ -1,1 +1,20 @@
-console.log('this is a test code');
+function add(n1: number, n2: number) {
+  // return type number is type inference'ed
+  return n1 + n2;
+}
+
+function printResult(num: number): void {
+  // returns nothing
+  console.log("Result: " + num);
+}
+
+function retUndefined(): undefined {
+  // returns Undefined
+  console.log("returns undefinde");
+  return undefined;
+}
+
+printResult(add(5, 12));
+retUndefined();
+console.log(printResult(add(5, 12))); // gives undefined
+// undefined is a type
